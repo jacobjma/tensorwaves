55e497c322ddb71cc84046172afbbb515656b1a9
Jacob Madsen <jacobmadsen@jacobma.pnm.univie.ac.at>
2019-04-11 10:06:55 +0200

    added install file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensorwaves',
+    version='0.1',
+    description='tensorwaves',
+    author='Jacob Madsen',
+    author_email='jacob.madsen@univie.ac.at',
+    packages=find_packages(),  # same as name
+    install_requires=[],  # external packages as dependencies
+)
